3fca79eccb487659028b66f152f5836fe33195dd
Bjorn Barrefors <barrefors@gmail.com>
2013-09-03 14:13:05 -0500

    cluster 23

--- data_analysis/power_check.r
+++ data_analysis/power_check.r
@@ -25,9 +25,9 @@ power_consumption <- as.vector(data[[4]])  # p is power consumption, column 3
 
 frequency <- frequency/1000             # Make f a measurement in GHz
 
-f <- frequency[1:21]
-T <- temperature[1:21]
-P <- power_consumption[1:21]
+f <- frequency[1:26]
+T <- temperature[1:26]
+P <- power_consumption[1:26]
 P <- (P / 10)				# convert power from 0.1W to 1W
 f2 <- f*f
 T2 <- T*T
